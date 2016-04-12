f870cf80c13d627159b16fe05d34bb00dda4d28e
Alain Keller <alain.keller@edu.tbz.ch>
2016-04-12 10:34:39 +0200

    Add friend, show friends

--- ZenChat/ZenChat/Chat/AllChatsViewModel.cs
+++ ZenChat/ZenChat/Chat/AllChatsViewModel.cs
@@ -2,11 +2,22 @@
 // All rights reserved
 
 using System.Collections.ObjectModel;
+using ZenChat.ZenChatService;
 
 namespace ZenChat.Chat
 {
 	internal class AllChatsViewModel
 	{
 		public ObservableCollection<ChatViewModel> MyChats { get; set; }
+
+		public AllChatsViewModel()
+		{
+			GetChats();
+		}
+
+		public void GetChats()
+		{
+			var client = new ZenChatServiceClient(ZenChatServiceClient.EndpointConfiguration.BasicHttpsBinding_ZenChatService);
+		}
 	}
 }
---
+++ ZenChat/ZenChat/Friends/FriendModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZenChat.Friends
+{
+	class FriendModel
+	{
+		private string _name;
+		private string _phoneNumber;
+
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+
+		public string PhoneNumber
+		{
+			get { return _phoneNumber; }
+			set { _phoneNumber = value; }
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="name">Name of the User</param>
+		/// <param name="phoneNumber">PhoneNumber of the User</param>
+		public FriendModel(string name, string phoneNumber)
+		{
+			Name = name;
+			PhoneNumber = phoneNumber;
+		}
+	}
+}
