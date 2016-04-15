fba45299872f8ba01cd875a27fe591fd4a00a608
Seraphin Elias Rihm <seraphin.rihm@edu.tbz.ch>
2016-04-15 21:21:33 +0200

    #Settings Code completed

--- ZenChat/ZenChat/Settings/SettingsViewModel.cs
+++ ZenChat/ZenChat/Settings/SettingsViewModel.cs
@@ -1,8 +1,10 @@
 ﻿// Copyright (c) 2016 
 // All rights reserved
 
+using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using Windows.UI.Popups;
 using Microsoft.Practices.Prism.Commands;
 using ZenChat.Annotations;
 using ZenChat.Models;
@@ -61,13 +63,62 @@ namespace ZenChat.Settings
 
 		private async void SaveChanges()
 		{
+
 			IsActive = true;
 			var client = new ZenChatServiceClient(ZenChatServiceClient.EndpointConfiguration.BasicHttpsBinding_ZenChatService);
-			await client.ChangeUsernameAsync(Session.UserID, Username);
-			var user = await client.ChangePhoneNumberAsync(Session.UserID, Phonenumber);
-			Session.PhoneNumber = Phonenumber = user.PhoneNumber;
-			Session.Username = Username = user.Name;
-			IsActive = false;
+			User user;
+
+			if (Phonenumber == Session.PhoneNumber && Username == Session.Username)
+			{
+
+				IsActive = false;
+				var dialog = new MessageDialog("Es gibt keine Änderungen");
+				await dialog.ShowAsync();
+				return;
+
+			}
+
+			if (Phonenumber != Session.PhoneNumber)
+			{
+
+				try
+				{
+
+					user = await client.ChangePhoneNumberAsync(Session.UserID, Phonenumber);
+					Session.PhoneNumber = Phonenumber = user.PhoneNumber;
+
+				}
+
+				catch (Exception e)
+				{
+
+					var dialog = new MessageDialog(e.Message);
+					await dialog.ShowAsync();
+
+				}
+			}
+			if (Username != Session.Username)
+			{
+
+				try
+				{
+
+					user = await client.ChangeUsernameAsync(Session.UserID, Username);
+					Session.Username = Username = user.Name;
+
+				}
+
+				catch (Exception e)
+				{
+
+					var dialog = new MessageDialog(e.Message);
+					await dialog.ShowAsync();
+
+				}
+			}
+
+				IsActive = false;
+
 		}
 
 		[NotifyPropertyChangedInvocator]
