51640ca13741733e65561d13b2a9cf19fd37c376
Seraphin Elias Rihm <seraphin.rihm@edu.tbz.ch>
2016-04-17 20:50:11 +0200

    #shit

---
+++ ZenChat/ZenChat/Friends/UserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Prism.Commands;
+
+namespace ZenChat.Friends
+{
+	class UserViewModel
+	{
+		public ZenChatService.User User { get; set; }
+		public DelegateCommand RemoveUsercommand{get; set;}
+
+		public UserViewModel()
+		{
+			RemoveUsercommand = new DelegateCommand();
+		}
+
+	}
